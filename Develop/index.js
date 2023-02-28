6903fc24da111af5b658cabf18f1b0344e204fcd
Brenden <brenthomas02@gmail.com>
2023-02-28 12:35:23 -0500

    Questions Added

---
+++ Develop/index.js
@@ -0,0 +1,21 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = 
+["What is the title of this application?",
+ "What is this application for?",
+ "How do you use the application?",
+ "How do you install this application?",
+ "How would you report an issue?",
+ "How would someone make contributions?",
+ "What instructions would you give to use this application?",
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
