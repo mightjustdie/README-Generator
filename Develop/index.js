96a0634b05d14119b8e0a93a771758f467759e11
Brenden <brenthomas02@gmail.com>
2023-03-02 10:32:05 -0500

    Fixed questions and added packages

--- Develop/index.js
+++ Develop/index.js
@@ -1,14 +1,36 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const markdown = require("./utils/generateMarkdown")
 // TODO: Create an array of questions for user input
-const questions = 
-["What is the title of this application?",
- "What is this application for?",
- "How do you use the application?",
- "How do you install this application?",
- "How would you report an issue?",
- "How would someone make contributions?",
- "What instructions would you give to use this application?",
+const questions = [
+{
+    message: "What is the title of this application?",
+    name: "title"
+},
+{
+    message: "What is this application for?",
+    name: "useage"
+},
+{
+    message: "How do you use the application?",
+    name: "howUse"
+},
+{
+    message: "How do you install this application?",
+    name: "install"
+},
+{
+    message: "How would you report an issue?",
+    name: "reporting"
+},
+{
+    message: "How would someone make contributions?",
+    name: "contributions"
+},
+{
+    message: "What instructions would you give to use this application?",
+    name: "instructions"
+},
 ];
 
 // TODO: Create a function to write README file
